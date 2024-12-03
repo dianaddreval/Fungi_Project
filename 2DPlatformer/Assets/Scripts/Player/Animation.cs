541deaa85c2d8a13b6572a5f7dbfd9df85395e6b
Diana Dreval <dia6626@icloud.com>
2024-12-03 22:37:42 +0700

    Add animations 2.0

--- 2DPlatformer/Assets/Scripts/Player/Animation.cs
+++
@@ -1,7 +0,0 @@
-using UnityEngine;
-
-public class Animation : MonoBehaviour
-{
-    [SerializeField] private Animator _animator;
-    private Player _player;
-}
--- 2DPlatformer/Assets/Scripts/Player/Player.cs
+++ 2DPlatformer/Assets/Scripts/Player/Player.cs
@@ -5,12 +5,16 @@ public class Player : MonoBehaviour
     [SerializeField] private float _speed = 3f;
     [SerializeField] private float _jumpForce = 5f;
     private bool _isGround;
+    private int _jumpCount;
+    private bool _isFalling = false;
     
+    private Ray _ray;
+    [SerializeField] private float _groundCheckRadius = 0.5f;
+    
+    private Animator _animator;
     private SpriteRenderer _sprite;
     private Rigidbody2D _rb2D;
     [SerializeField] private LayerMask _mask;
-    Ray _ray;
-    [SerializeField] private float _groundCheckRadius = 0.5f;
     
     public static Player Instance;
 
@@ -20,25 +24,23 @@ public class Player : MonoBehaviour
     {
         _sprite = GetComponentInChildren<SpriteRenderer>(); 
         _rb2D = GetComponent<Rigidbody2D>();
+        _animator = GetComponent<Animator>();
+
     }
 
     void Update()
     {
         DrawLine();
+        VerticalSpeed();
+        CheckGround();
     }
 
-    internal void Move(float horizontalInput)
+    internal void Move(float value)
     {
-        _rb2D.linearVelocity = new Vector2(horizontalInput * _speed, _rb2D.linearVelocity.y);
+        _rb2D.linearVelocity = new Vector2(value * _speed, _rb2D.linearVelocity.y);
+        _animator.SetBool("IsRun", Mathf.Abs(_rb2D.linearVelocity.x) > 0.01f);
+        _sprite.flipX = value < 0 ? true : false;
         
-        if (_rb2D.linearVelocity.x < 0)
-        {
-            _sprite.flipX = true;
-        }
-        else if(_rb2D.linearVelocity.x > 0)
-        {
-            _sprite.flipX = false;
-        }
     }
 
     internal void Jump()
@@ -49,12 +51,29 @@ public class Player : MonoBehaviour
         {
             _rb2D.linearVelocity = new Vector2(_rb2D.linearVelocity.x, _jumpForce);
         }
+        
+        _animator.SetTrigger("Jump");
+    }
+
+    private void VerticalSpeed()
+    {
+        _animator.SetBool("IsFall", _isFalling);
+        if (_rb2D.linearVelocity.y < 0)
+        {
+            _isFalling = true;
+        }
     }
     
     public bool CheckGround()
     {
         var hit = Physics2D.Raycast(transform.position, Vector2.down, _groundCheckRadius, _mask);
-        return hit.collider != null;
+        
+        if (hit.collider != null)
+        {
+            _isFalling = false;
+            return true;
+        }
+        return false;
     }
 
     private void DrawLine()
